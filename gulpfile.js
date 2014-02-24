8885da2df9953ae0ca72c18ae49380e32ad6e641
ArmNo <monkeyarmno@gmail.com>
2014-02-24 17:16:54 +0700

    use gulp-ruby-sass instead of gulp-sass for sass compiling task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
+ // gulpfile.js
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
-gulp.task('lorem', function() {
-  console.log('lorem ipsum');
+gulp.task('css', function(){
+  return gulp.src('./sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('index.html', ['lorem']);
+  gulp.watch('sass/**/*.sass', ['css']);
 });
 
 gulp.task('default', ['watch']);
