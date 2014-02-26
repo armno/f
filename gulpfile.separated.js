4b2a236f874d95987e686a24e9e18a78162dbf2a
ArmNo <monkeyarmno@gmail.com>
2014-02-26 17:38:12 +0700

    create another version of gulpfile

--- gulpfile.separated.js
+++ gulpfile.separated.js
@@ -1,28 +1,36 @@
-// gulpfile.js
+ // gulpfile.js
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('build', function() {
-    gulp.src('./sass/**/*.sass')
+gulp.task('css', function(){
+  return gulp.src('./sass/**/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('./css'));
+});
 
-    gulp.src([
+gulp.task('js', function(){
+  return gulp.src([
         './src/js/model.js',
         './src/js/controller.js'
         ])
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./js'));
+});
 
-    gulp.src(['./src/index.html'])
+gulp.task('html', function() {
+  return gulp.src(['./src/index.html'])
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', function() {
-  gulp.watch('./src/**/*', ['build']);
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.sass', ['css']);
+  gulp.watch('./src/**/*.js', ['js']);
+  gulp.watch('./src/index.html', ['html']);
 });
+
+gulp.task('default', ['css', 'js', 'html', 'watch']);
