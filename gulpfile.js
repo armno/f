1707446a3e561154f17dee7b9b8ecd5ca8a9b4b8
ArmNo <monkeyarmno@gmail.com>
2014-02-26 16:46:36 +0700

    added html minify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('css', function(){
   return gulp.src('./sass/**/*.sass')
@@ -20,9 +21,16 @@ gulp.task('js', function(){
         .pipe(gulp.dest('./js'));
 });
 
+gulp.task('html', function() {
+  return gulp.src(['./src/index.html'])
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('.'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./sass/**/*.sass', ['css']);
   gulp.watch('./src/**/*.js', ['js']);
+  gulp.watch('./src/index.html', ['html']);
 });
 
 gulp.task('default', ['watch']);
